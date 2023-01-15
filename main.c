e403b839383f3a3e283efdb4af8667ecc37a800e
duda <lacwerda@gmail.com>
2023-01-15 18:37:36 -0300

    rand tweet

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 #include <pthread.h>
 #include <semaphore.h>
 
@@ -7,30 +9,32 @@
 #define USERNAME_FILE "twitter-info/usernames.txt"
 
 #define TWEET_SIZE 280 + 2
-#define QUANT_TWEETS 50
+#define MAX_QUANT_TWEETS 50
 #define USERNAME_SIZE 15 + 2
-#define QUANT_USERNAMES 280
+#define MAX_QUANT_USERNAMES 280
 
 #define TRUE 1
 #define PAUSA_PARA_LEITURA 10
 #define QUANTIDADE_MAXIMA_TWEETS 20
 
-char (*tweets)[TWEET_SIZE] = NULL;
-char (*usernames)[QUANT_USERNAMES] = NULL;
+char (*tweets)[TWEET_SIZE] = NULL, (*usernames)[MAX_QUANT_USERNAMES] = NULL;
 
-int index_users = 0, index_tweets = 0;
+int quant_users = 0, quant_tweets = 0;
 
 int capacidade_atual = QUANTIDADE_MAXIMA_TWEETS;
 
 void open_twitter_info();
 void print_tweet(char *user, char *tweet);
+char* pensar_em_tweet();
 
 int main() {
-
     open_twitter_info();
 
     // INICIALIZE THREADS AQUI
     // INICIALIZE THREADS AQUI
+
+    char* tweet = pensar_em_tweet();
+    print_tweet("@beatriz", tweet);
 
     free(tweets);
     free(usernames);
@@ -42,23 +46,26 @@ void open_twitter_info() {
     FILE *tweet_file = fopen(TWEET_FILE, "r");
     FILE *username_file = fopen(USERNAME_FILE, "r");
 
-    if (!(tweets = malloc(QUANT_TWEETS * sizeof *tweets)) || !(usernames = malloc(QUANT_USERNAMES * sizeof *usernames)))
+    if (!(tweets = malloc(MAX_QUANT_TWEETS * sizeof *tweets)))
         exit(EXIT_FAILURE);
 
-    while (index_tweets < QUANT_TWEETS && fgets(tweets[index_tweets], TWEET_SIZE, tweet_file))
+    if (!(usernames = malloc(MAX_QUANT_USERNAMES * sizeof *usernames)))
+        exit(EXIT_FAILURE);
+
+    while (quant_tweets < MAX_QUANT_TWEETS && fgets(tweets[quant_tweets], TWEET_SIZE, tweet_file))
     {
-        char *p = tweets[index_tweets];
+        char *p = tweets[quant_tweets];
         for (; *p && *p != '\n'; p++) {}
         *p = 0;
-        index_tweets++;
+        quant_tweets++;
     }
 
-    while (index_users < QUANT_USERNAMES && fgets(usernames[index_users], USERNAME_SIZE, username_file))
+    while (quant_users < MAX_QUANT_USERNAMES && fgets(usernames[quant_users], USERNAME_SIZE, username_file))
     {
-        char *z = usernames[index_users];
+        char *z = usernames[quant_users];
         for (; *z && *z != '\n'; z++) {}
         *z = 0;
-        index_users++;
+        quant_users++;
     }
 
     fclose(tweet_file);
@@ -66,6 +73,12 @@ void open_twitter_info() {
 }
 
 void print_tweet(char *user, char *tweet) {
-    printf("%s tweetou:\n\n", user);
-    printf("   '%s'\n\n", tweet);
+    printf("\e[1m%s\e[m tweetou:\n\n   '%s'\n\n", user, tweet);
+}
+
+char* pensar_em_tweet() {
+    printf("fulano está pensando em um tweet para escrever...\n\n");
+    srand(time(NULL));
+    sleep(2);
+    return tweets[rand() % quant_tweets];
 }
